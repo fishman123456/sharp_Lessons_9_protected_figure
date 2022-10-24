eb3ad7bfacf42f1d02128be89cffffda208028be
F_D <den9152@yandex.ru>
2022-10-24 21:00:26 +0700

    Initial commit

---
+++ Circle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharp_Lessons_9_protected_figure
+{
+    public class Circle : Employe
+    {
+        public int x;
+        public int y;
+
+    }
+}
---
+++ Employe.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharp_Lessons_9_protected_figure
+{
+    public class Employe
+    {
+        protected double _area;
+        protected double _perimeter;
+        protected string _figure;
+
+        public Employe ()
+        {
+            _area = 10;
+            _perimeter = 10;
+            _figure = "квадрат";
+        }
+        public Employe (double area, double perimetr, string figure)
+        {
+            this._area = area;
+            this._perimeter = perimetr;
+            this._figure = figure;
+        }
+       public void Print ()
+        {
+            Console.WriteLine ("\tполощадь\t" + _area + "\tпериметр\t" +
+                _perimeter + "\tимя фигуры\t" + _figure + "\n");
+        }
+           
+    }
+}
