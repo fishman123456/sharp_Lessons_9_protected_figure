d7c2f7328906fc99537731a6ad1536120bc02859
fishman123456 <den9152@yandex.ru>
2022-10-26 05:52:19 +0700

    круг доделан
    остальное не успеваю

--- Circle.cs
+++ Circle.cs
@@ -11,25 +11,30 @@ namespace sharp_Lessons_9_protected_figure
         public int x;
         public int y;
         public int radius;
+       
 
         public Circle()
         {
             this.x = 0;
             this.y = 0;
             this.radius = 0;
+            this._figure = "Окружность";
 
         }
-        public Circle (int x1, int y1, int radius1)
+        public Circle (int x1, int y1, int radius1,string figure)
         {
             x = x1; 
             y = y1;
             radius = radius1;
+            _figure = "Класс Circle ";
+            
         }
         public double area()
         {
             _area = 3.14 * (radius*radius);
             return _area;
         }
+      
     }
   
 }
